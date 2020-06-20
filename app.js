a2ac535f3d224fef4f2d6684b4f38d2a06ae7147
rd <rdding1123@gmail.com>
2020-06-20 16:21:21 +0800

    Add facebook login

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('passport')
 const app = express()
-
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('./models')
 const Todo = db.Todo
 const User = db.User
 
 const port = 3000
+
+
 
 app.engine('handlebars', exphb({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -34,6 +38,7 @@ app.get('/', (req, res, next) => {
 app.use('/', require('./routes/home'))
 app.use('/users', require('./routes/user'))
 app.use('/todos', require('./routes/todo'))
+app.use('/auth', require('./routes/auth'))
 
 app.listen(port, () => {
   console.log('server start!')
--- config/passport.js
+++ config/passport.js
@@ -2,6 +2,7 @@ const LocalStrategy = require('passport-local').Strategy
 const db = require('../models')
 const User = db.User
 const bcrypt = require('bcryptjs')
+const facebookStrategy = require('passport-facebook').Strategy
 
 module.exports = passport => {
   passport.use(
@@ -21,6 +22,43 @@ module.exports = passport => {
         })
     })
   )
+
+  passport.use(new facebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  },
+    function (accessToken, refreshToken, profile, done) {
+      User.findOne({
+        where: {
+          email: profile._json.email
+        }
+      }).then(user => {
+        if (!user) {
+          var randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(randomPassword, salt, (err, hash) => {
+              var newUser = new User({
+                name: profile._json.name,
+                email: profile._json.email,
+                password: hash
+              })
+              newUser.save()
+                .then(user => {
+                  return done(null, user)
+                })
+                .catch(err => {
+                  console.log(err)
+                })
+            })
+          })
+        } else {
+          return done(null, user)
+        }
+      })
+    }
+  ));
 
   passport.serializeUser((user, done) => {
     done(null, user.id)
